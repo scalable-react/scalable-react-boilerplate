f1c1f3da010576af6453a5a9bc778629c08930ff
Ryan Collins <rccproductions@mac.com>
2016-06-27 14:27:56 -0400

    Feat: add reducers for errors and messages

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -4,9 +4,13 @@ import { reducer as formReducer } from 'redux-form';
 import { reducer as toastrReducer } from 'react-redux-toastr';
 
 // Import the various reducers here:
+import errors from './errors';
+import messages from './messages';
 
 const rootReducer = combineReducers({
   // Apply all of the reducers here.
+  errors,
+  messages,
   routing: routerReducer,
   form: formReducer,
   toastr: toastrReducer
