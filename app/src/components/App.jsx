44788e10654a390948f32cdd8c8fb6e6e3f802a1
Ryan Collins <rccproductions@mac.com>
2016-06-27 14:36:12 -0400

    Feat: add comments to the app regarding redux methods

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -4,11 +4,14 @@ import * as actionCreators from '../actions/actionCreators';
 import Main from './Main';
 
 // Map the global state to global props here.
+// See: https://egghead.io/lessons/javascript-redux-generating-containers-with-connect-from-react-redux-visibletodolist
 const mapStateToProps = (state) => ({
   messages: state.messages,
   errors: state.errors
 });
 
+// Map the dispatch and bind the action creators.
+// See: http://redux.js.org/docs/api/bindActionCreators.html
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(
     actionCreators,
@@ -16,6 +19,8 @@ function mapDispatchToProps(dispatch) {
   );
 }
 
+// Use connect both here and in your components.
+// See: https://egghead.io/lessons/javascript-redux-generating-containers-with-connect-from-react-redux-visibletodolist
 const App = connect(
   mapStateToProps,
   mapDispatchToProps
