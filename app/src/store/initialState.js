2438dfd1278a4ce59b4081cac0f51b38da4bb0bb
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-07-10 14:34:19 -0400

    Fix: update initial state removing errors and messages and adding amazing Component

--- app/src/store/initialState.js
+++ app/src/store/initialState.js
@@ -1,12 +1,9 @@
 const initalState = {
-  // Most apps have the need to show messages and errors.
-  // Just add an array or object for each model.
-  messages: {
-    alertVisible: false,
-    myModel: []
-  },
-  errors: {
-    myModel: []
+  amazingComponent: {
+    errors: [],
+    messages: [],
+    foo: '',
+    bar: ''
   }
 };
 
