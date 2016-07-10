ea251c2f723b8c64fd350aecdee52bbadea0a4d4
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-07-10 14:22:14 -0400

    Feat: add separate constants file for amazingComponent

---
+++ app/src/constants/amazingComponent.js
@@ -0,0 +1,5 @@
+/* Constants for displaying and clearing errors and messages for amazing component. */
+export const AMAZING_COMPONENT_ERROR = 'AMAZING_COMPONENT_ERROR';
+export const AMAZING_COMPONENT_MESSAGE = 'AMAZING_COMPONENT_MESSAGE';
+export const CLEAR_AMAZING_COMPONENT_ERROR = 'CLEAR_AMAZING_COMPONENT_ERROR';
+export const CLEAR_AMAZING_COMPONENT_MESSAGE = 'CLEAR_AMAZING_COMPONENT_MESSAGE';
