b11b8fadb6a5e789843d3d805fec690ac1172659
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-07-24 01:07:55 -0400

    Feat: update Landing Page with description of what Pages are for

--- app/src/pages/LandingPage/LandingPage.js
+++ app/src/pages/LandingPage/LandingPage.js
@@ -4,6 +4,8 @@ import styles from './LandingPage.module.scss';
 // Example to import a component using ES6 destructuring.
 import { MyAmazingContainer } from '../../containers';
 
+// Pages map directly to Routes, i.e. one page equals on Route
+// Handler that maps to a route in /utils/routes
 const LandingPage = (props) => (
   <div className={styles.container}>
     <MyAmazingContainer
