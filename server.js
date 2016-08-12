f7bed2a1f67c757a828d849a0b55971e173e91f0
adai183 <andreas.daiminger@gmail.com>
2016-08-12 19:23:04 +0200

    deploy

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.NODE_ENV || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/app/build'));
+app.use(express.static(__dirname + '/app/dist'));
 
-app.get('/', function (request, response) {
-	response.render('/app/build/index.html');
+
+app.get('/', function(request, response) {
+  response.render('app/dist/index');
 });
 
-
-app.listen(app.get('port'), function () {
-	console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
