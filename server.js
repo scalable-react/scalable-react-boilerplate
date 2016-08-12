8b442cf6cf33be7a1ea69f72631cdf2b7837b99c
adai183 <andreas.daiminger@gmail.com>
2016-08-12 19:10:08 +0200

    prepare deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.NODE_ENV || 5000));
 
 app.use(express.static(__dirname + '/app/build'));
 
