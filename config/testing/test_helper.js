2ceeb9c25e98e714515a1035a085979a9835efc4
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-08-17 16:05:53 -0400

    Revert "Fix: add missing package and rename webpack test configuration file"

--- config/testing/test_helper.js
+++ config/testing/test_helper.js
@@ -1,9 +1,12 @@
-import 'babel-polyfill';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
 // Load up every single JS file under the source directory except for
 // reducers, routes and store files.
-const context = require.context('../../app/src', true, /^^((?!(app|reducers|routes)).)*\.js$/);
+const context = require.context(
+  '../../app/src',
+  true,
+  /^^((?!(app|reducers|routes|store)).)*\.js$/
+);
 context.keys().forEach(context);
