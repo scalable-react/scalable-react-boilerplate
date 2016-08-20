773102c92ea41c832ab25a450a27ef3a912a75b3
Ryan Collins <rccproductions@mac.com>
2016-08-19 20:34:30 -0400

    Fix: resolve merge conflicts in test_helper and webpack config

--- config/testing/test_helper.js
+++ config/testing/test_helper.js
@@ -1,16 +1,9 @@
+import 'babel-polyfill';
 import chai from 'chai';
 import chaiEnzyme from 'chai-enzyme';
 chai.use(chaiEnzyme());
 
 // Load up every single JS file under the source directory except for
 // reducers, routes and store files.
-<<<<<<< HEAD
 const context = require.context('../../app/src', true, /([^\a]+).test\.js$/);
-=======
-const context = require.context(
-  '../../app/src',
-  true,
-  /^^((?!(app|reducers|routes|store)).)*\.js$/
-);
->>>>>>> c6f5eaf35b59c90e3eb65fffdd960e4e92aae81c
 context.keys().forEach(context);
--- config/webpack/webpack.test.config.js
+++ config/webpack/webpack.test.config.js
@@ -1,9 +1,63 @@
-import 'babel-polyfill';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-chai.use(chaiEnzyme());
+const path = require('path');
+const webpack = require('webpack');
+const ROOT_PATH = path.resolve(__dirname);
+const HtmlwebpackPlugin = require('html-webpack-plugin');
 
-// Load up every single JS file under the source directory except for
-// reducers, routes and store files.
-const context = require.context('../../app/src', true, /([^\a]+).test\.js$/);
-context.keys().forEach(context);
+const modules = [
+  'app/src',
+  'node_modules',
+];
+
+module.exports = {
+  devtool: 'inline-source-map',
+  isparta: {
+    babel: {
+      presets: ['es2015', 'react', 'stage-0'],
+    },
+  },
+  module: {
+    noParse: [
+      // If libraries complain about babel, put them here.
+    ],
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'isparta',
+        include: path.resolve('app/src'),
+      },
+    ],
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader' },
+      { test: /\.scss$/, loader: 'null-loader' },
+      { test: /\.js$/, loader: 'babel', exclude: [/node_modules/] },
+      { test: /\.jpe?g$|\.gif$|\.png$/i, loader: 'null-loader' },
+    ],
+  },
+  plugins: [
+    new HtmlwebpackPlugin({
+      title: 'Scalable React Boilerplate',
+      template: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+  externals: {
+    jsdom: 'window',
+    'react/addons': true,
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': 'window',
+  },
+  resolve: {
+    modulesDirectoris: modules,
+    modules,
+    alias: {
+      sinon: 'sinon/pkg/sinon',
+      components: path.resolve(ROOT_PATH, '../../app/src/components'),
+      containers: path.resolve(ROOT_PATH, '../../app/src/containers'),
+      pages: path.resolve(ROOT_PATH, '../../app/src/pages'),
+    },
+  },
+};
