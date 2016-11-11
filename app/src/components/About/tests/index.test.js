eddfc0754551a85bc8a5b92c24ab67728df5349f
Ryan Collins <rccproductions@mac.com>
2016-11-10 23:31:33 -0500

    feat: Update test suite, adding babelrc and updated jest package

--- app/src/components/About/tests/index.test.js
+++ app/src/components/About/tests/index.test.js
@@ -3,10 +3,21 @@ import { shallow } from 'enzyme';
 import { shallowToJson } from 'enzyme-to-json';
 import React from 'react';
 
+const links = [
+  {
+    url: 'https://github.com/udacityalumni/alumni-client',
+    name: 'Udacity Alumni Web App',
+  },
+  {
+    url: 'https://github.com/RyanCCollins/react-weekly',
+    name: 'React Weekly',
+  },
+];
+
 describe('<About />', () => {
   it('should render with default props', () => {
     const wrapper = shallow(
-      <About />
+      <About links={links} />
     );
     expect(shallowToJson(wrapper)).toMatchSnapshot();
   });
