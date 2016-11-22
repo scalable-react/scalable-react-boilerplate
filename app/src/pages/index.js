087597608e115e1670d17108f4dc2c70d7ae0b6c
Ryan Collins <RyanCCollins@users.noreply.github.com>
2016-11-22 17:55:12 -0500

    fix: Remove export from helloPage

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,5 +1,4 @@
 /* GENERATOR: Assemble all pages for export */
 export AboutPage from './AboutPage/index';
-export HelloPage from './HelloPage/index';
 export NotFoundPage from './NotFoundPage/index';
 export LandingPage from './LandingPage/index';
