5204557f66283fc7abf1f00966c9123bb06612f5
Ryan Collins <rccproductions@mac.com>
2016-12-16 16:21:20 -0500

    Feat: update api for creating apollo client

--- app/src/apolloClient.js
+++ app/src/apolloClient.js
@@ -7,10 +7,9 @@ import { BASE_URL } from 'config';
 const url = `${BASE_URL}graphql`;
 
 const client = new ApolloClient({
-  networkInterface: createNetworkInterface({
-    uri: url,
-  }),
+  networkInterface: createNetworkInterface(url),
   initialState: typeof window !== 'undefined' ? window.__APOLLO_STATE__ : null,
+  ssrForceFetchDelay: 100,
   queryTransformer: addTypeName,
 });
 
